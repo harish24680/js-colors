c83b75090b24a9b805905f10d2bd6cbfeb546717
harish24680 <60013824+harish24680@users.noreply.github.com>
2020-02-12 10:19:29 -0600

    Commit js

--- main.js
+++ main.js
@@ -14,7 +14,9 @@ document.querySelector('#blue').addEventListener('click', updateColor)
 document.querySelector('#green').addEventListener('click', updateColor)
 document.querySelector('#red').addEventListener('touchend', updateColor)
 document.querySelector('#blue').addEventListener('touchend', updateColor)
-document.querySelector('#green').addEventListener('touchend', updateColor)
+document.querySelector('#green').addEventListener('touchend', updateColor
+ document.querySelector('#foot').addEventListener('click', alert('hello'))
+
 
 // call the handler 
 updateColor() 
